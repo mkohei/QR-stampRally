d26d0432a251779b06cf3507bbe5f11dad329385
mkohei <baberugaberiimeronnv5@gmail.com>
2018-02-28 01:10:30 +0900

    とりあえず、っぽい機能の実装

---
+++ test-develop/js/c.js
@@ -0,0 +1,31 @@
+// QUERY KEY
+QUERY_KEY = "stamp";
+// STAMP KEY (QUERY_VALUE)
+STAMP_KEY_1 = "k1";
+STAMP_KEY_2 = "k2";
+STAMP_KEY_3 = "k3";
+STAMP_KEY_ARRAY = [
+    STAMP_KEY_1,
+    STAMP_KEY_2,
+    STAMP_KEY_3
+];
+
+// IMG
+IMG_DIR = 'img/'
+    // IMG ID
+IMG_ID1 = 'id1';
+IMG_ID2 = 'id2';
+IMG_ID3 = 'id3';
+// IMG FILE - ID
+IMG_ARRAY = {
+    '1.png': IMG_ID1,
+    '2.png': IMG_ID2,
+    '3.png': IMG_ID3,
+}
+
+// STAMP_KEY - IMG ID
+STAMP_KEY2IMG_ID = {
+    [STAMP_KEY_1]: IMG_ID1,
+    [STAMP_KEY_2]: IMG_ID2,
+    [STAMP_KEY_3]: IMG_ID3,
+}
