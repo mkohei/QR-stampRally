52f3612e2f188f5037283c732e731c888627bf18
mkohei <baberugaberiimeronnv5@gmail.com>
2018-03-05 02:42:52 +0900

    QRまとめページ

--- test-dev2/js/c.js
+++ test-dev2/js/c.js
@@ -29,7 +29,24 @@ function STAMP_ID2IMG_ID(stampID) {
 
 function STAMP_ID2HP(stampID) {
     return {
-        "hamamura": "http://hamalab.info/"
+        "hamamura": "http://hamalab.info/",
+        "iwata": "http://www.lab.kochi-tech.ac.jp/iwatalab/",
+        "shikida": "https://www.shikidalab.org/",
+        "shinomori": "http://www.lab.kochi-tech.ac.jp/shinolab/",
+        "shimizu": "http://www.smzl.info/smzl/",
+        "nakahara": "http://www.lab.kochi-tech.ac.jp/knkhrlab/",
+        "fukumoto": "http://www.lab.kochi-tech.ac.jp/fukulab/",
+        "yokoyama": "http://yokolab.kochi-tech.ac.jp/",
+        "ren": "http://xrenlab.com/",
+        "ugawa": "http://pl.info.kochi-tech.ac.jp/",
+        "kadota": "https://www.facebook.com/HKlabKUT/",
+        "kurihara": "http://krlab.info.kochi-tech.ac.jp/",
+        "shigemasu": "http://shigelab.com/",
+        "takada": "http://www.info.kochi-tech.ac.jp/y-takata/",
+        "matsuzaki": "http://ipl.info.kochi-tech.ac.jp/matsuzaki-lab/home.html",
+        "mendori": "http://www.lab.kochi-tech.ac.jp/menken/",
+        "yoshida": "http://www.lab.kochi-tech.ac.jp/yoshilab/index.html",
+        "ueda": "http://www.scinfo.kochi-tech.ac.jp/k-ueda/"
     }[stampID];
 }
 
